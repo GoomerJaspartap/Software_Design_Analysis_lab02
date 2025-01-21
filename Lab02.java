d78bb229e939d475e88ce47d949db8e6b7ee93c3
GoomerJaspartap <jaspartapsingh2004@gmail.com>
2025-01-21 12:22:11 -0500

    added return message

--- Lab02.java
+++ Lab02.java
@@ -6,18 +6,21 @@ public class Lab02{
     static Scanner scan = new Scanner(System.in);
    public static void main(String[] args) throws FileNotFoundException
     {
-        ArrayList<Book> books = new ArrayList<Book>(); // Create an ArrayList object
+       
+
+    }
+    public static ArrayList<Book> CSV_DATA () throws FileNotFoundException{
+        ArrayList<Book> books_data = new ArrayList<Book>(); // Create an ArrayList object
         File data = new File("books.csv");
         try (Scanner file_scan = new Scanner(data)) {
             while(file_scan.hasNextLine()){
                 String data_val = file_scan.nextLine();
                 String[] fields = data_val.split(",");
                 Book book_obj = new Book(fields[0],fields[1],fields[2],Integer.parseInt(fields[3]) ,fields[4]);
-                books.add(book_obj);
+                books_data.add(book_obj);
             }
 
         }
-        System.out.println(books);
-
+return books_data;
     }
 }
