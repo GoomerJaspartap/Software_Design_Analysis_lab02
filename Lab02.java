48464e8b888b36920a09bb6e07a169df7cd64e47
GoomerJaspartap <jaspartapsingh2004@gmail.com>
2025-01-21 11:58:40 -0500

    backend

---
+++ Lab02.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Lab02{
+    static Scanner scan = new Scanner(System.in);
+   public static void main(String[] args) throws FileNotFoundException
+    {
+        ArrayList<Book> books = new ArrayList<Book>(); // Create an ArrayList object
+        File data = new File("books.csv");
+        try (Scanner file_scan = new Scanner(data)) {
+            while(file_scan.hasNextLine()){
+                String data_val = file_scan.nextLine();
+                String[] fields = data_val.split(",");
+                Book book_obj = new Book(fields[0],fields[1],fields[2],Integer.parseInt(fields[3]) ,fields[4]);
+                books.add(book_obj);
+            }
+
+        }
+        System.out.println(books);
+
+    }
+}
